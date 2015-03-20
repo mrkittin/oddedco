d5416ff247a97e97f822ebb16e4a29f7c11778a9
blackswanny <blackswanny@gmail.com>
2015-03-20 12:02:20 -0700

    layout for project

--- src/js/controllers/ProjectController.js
+++ src/js/controllers/ProjectController.js
@@ -1,3 +1,31 @@
 angular.module("ctw").controller("ProjectController", function($scope) {
-  $scope.message = 'you are on project page';
+  var response = {
+    projectTitle: 'Title',
+    projectAuthor: 'Author',
+    projectMediaUrl: 'https://placekitten.com/g/200/300',
+    participants: ['Developer', 'Creator'],
+    categories: ['Lifestyle', 'Social'],
+    postedOn: new Date(),
+    comments: [
+      {
+        author: 'Mike',
+        text: 'I am curious about this project',
+        title: '',
+        date: new Date()
+      },
+      {
+        author: 'Nick',
+        text: 'i think nothing',
+        title: '',
+        date: new Date()
+      },
+      {
+        author: 'John',
+        text: 'It is wonderful',
+        title: '',
+        date: new Date()
+      }
+    ]
+  };
+  $scope.data = response;
 });
